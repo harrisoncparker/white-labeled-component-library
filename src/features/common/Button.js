2705102dc0d5ab18d3130173ed37d1e2eb519bec
Harrison Parker <hcp111@me.com>
2019-06-19 17:37:26 +0100

    Add button stories

--- src/features/common/Button.js
+++ src/features/common/Button.js
@@ -1,17 +1,103 @@
 import React from "react";
+import '../../styles/app.scss';
+import SVG from 'react-inlinesvg';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
+
+	/**
+	Variables
+	 */
+	--grey-mid: ${props => props.theme.greyMid};
+	--grey-light: ${props => props.theme.greyLight};
+	
+	/**
+	Core
+	 */
+	text-align: center;
 	display: inline-block;
 	border-radius: 0;
-	padding: 1rem;
-	border: none;
-	background-color: #eee;
-	color: #222;
+	padding: 14px;
+	font-size: 16px;
+	border: 1px solid var(--grey-mid);
+	background-color: white;
+	color: var(--grey-mid);
+	font-weight: 300;
+	width: 260px;
+	cursor: pointer;
+	text-decoration: none;
+	line-height: 1.5;
+	letter-spacing: 1px;
+	transition: .1s ease-out;
 	
-	&.inverted {
-		background-color: #222;
-		color: #eee;
+	&:hover{ 
+		background-color: var(--grey-mid);
+		color: var(--grey-light);
+	}
+	
+	/**
+	Icon
+	 */
+	.button__icon {
+		display: inline-block;
+		width: 16px;
+		height: 16px;
+		margin-right: .5rem;
+		transform: translateY(3px);
+		
+		svg {
+			transition: .1s ease-out;
+			width: 100%;
+			height: 100%;
+			fill: var(--grey-mid);
+			stroke: var(--grey-mid);
+		}
+	}
+	
+	&:hover {
+		.button__icon {
+			svg {
+				fill: var(--grey-light);
+				stroke: var(--grey-light);
+			}
+		}
+	}
+	
+	/**
+	Loading
+	 */
+	&.loading {
+		position: relative;
+		cursor: default;
+		pointer-events: none;
+	
+		&::after {
+			box-sizing: border-box;
+			content: '';
+			display: inline-block;
+			width: 14px;
+			height: 14px;
+			border-top: 1px solid var(--grey-mid);
+			border-left: 1px solid var(--grey-mid);
+			border-bottom: 1px solid var(--grey-mid);
+			border-right: 1px solid white;
+			border-radius: 50%;
+			margin-left: .5rem;
+			animation: 1s linear infinite rotate;
+		}
+		&:hover{ 
+			background-color: white;
+			color: var(--grey-mid);
+		}
+	}
+	
+	@keyframes rotate {
+		from {
+			transform: rotate(0deg)
+		}
+		to {
+			transform: rotate(360deg)
+		}
 	}
 `;
 
@@ -23,9 +109,16 @@ const Button = props => {
 		className += props.className;
 	}
 
-	return (
-		<StyledButton className={className}>{props.label}</StyledButton>
-	);
+	let icon = '';
+	if (props.iconPath) {
+		icon = <SVG className={`button__icon`} src={props.iconPath}/>;
+	}
+
+	if (props.href) {
+		return <StyledButton as="a" href={props.href} className={className}>{icon}{props.label}</StyledButton>
+	}
+
+	return <StyledButton className={className}>{icon}{props.label}</StyledButton>
 };
 
 export default Button;
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,11 +1,30 @@
 import React from "react";
-import {storiesOf} from "@storybook/react";
+import {storiesOf, addDecorator} from "@storybook/react";
 import Button from "../features/common/Button";
+import {ThemeProvider} from 'styled-components';
+
+const theme = {
+	greyDark: '#696969',
+	greyMid: '#767675',
+	greyLight: '#fafafa'
+};
+
+addDecorator(storyFn => (
+	<ThemeProvider theme={theme}>
+		{storyFn()}
+	</ThemeProvider>
+));
 
 storiesOf("Button", module)
-	.add("With text", () => (
-		<Button label={`some text`}/>
+	.add("Default (Button)", () => (
+		<Button label={`Button CTA`}/>
 	))
-	.add("With text (inverted)", () => (
-		<Button label={`some text`} className={`inverted`}/>
+	.add("Default (Link)", () => (
+		<Button href={`#`} label={`Button CTA`}/>
+	))
+	.add("With Icon", () => (
+		<Button href={`#`} iconPath={require(`../images/svg/icon.svg`)} label={`Button CTA`}/>
+	))
+	.add("With Loading", () => (
+		<Button className={`loading`} href={`#`} label={`Loading...`}/>
 	));
